bc19c318dfe4d917dbc842038fdf8f01c422ea71
Johnny McGee <johnnymcgee.dev@gmail.com>
2023-03-15 12:56:51 -0500

    Add Button component in lib

---
+++ src/lib/Button.svelte
@@ -0,0 +1,13 @@
+<script>
+  // your script goes here
+</script>
+
+<!-- markup (zero or more items) goes here -->
+<button class="cowabunga-btn">Cowabunga</button>
+
+<style>
+  .cowabunga-btn {
+    padding: 12px 24px;
+    border-radius: 24px;
+  }
+</style>
